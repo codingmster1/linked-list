c44271c48a5ac4e549525e3118b41335dad6133b
Jeff <doylejeff98@gmail.com>
2023-09-07 12:13:42 -0400

    commented out code

--- LinkedList.js
+++ LinkedList.js
@@ -10,17 +10,27 @@ class LinkedList {
         this.length++
 
     }
+
+    getByIndex(index) {
+        if (index < 0 || index >= this.length) return null
+    }
 }
 
-{
-
-}
 
 class LinkedListNode {
     constructor(value, next) {
         this.value = value 
         this.next = next
     }
+}
+
+LinkedList.fromValues = function(...values) {
+    const ll = newLinkedList()
+    for (let i = values.length -1; i >= 0; i--) {
+        ll.insertAtHead(values[i])
+
+    }
+    return ll
 }
 
 module.exports = LinkedList
--- LinkedList.test.js
+++ LinkedList.test.js
@@ -1,8 +1,8 @@
 /*const { exportAllDeclaration } = require('@babel/types')
 const { default: test } = require('node:test')
 const { describe } = require('yargs')*/
-const { default: test } = require('node:test')
-const { describe } = require('yargs')
+/*const { default: test } = require('node:test')
+const { describe } = require('yargs')*/
 const LinkedList = require('./LinkedList')
 
 describe('#insertAtHead', () => {
@@ -21,7 +21,34 @@ describe('#insertAtHead', () => {
 describe('#getByIndex', () => {
     describe('with index less than 0', () => {
         test('it returns null', () => {
-            const ll = new LinkedList()
+            const ll = LinkedList.fromValues(10, 20)
+
+            expect(ll.getByIndex(-1)).toBeNull()
+        })
+    })
+
+    describe('with index greater than list length', () => {
+        test('it returns null', () => {
+            const ll = LinkedList.fromValues(10, 20)
+
+            expect(ll.getByIndex(5)).toBeNull()
+        })
+    })
+
+    describe('with index 0', () => {
+        test('it returns the head', () => {
+            const ll = LinkedList.fromValues(10, 20)
+
+            expect(ll.getByIndex(0).value).toBe(10)
+        })
+    })
+
+    describe('with index in the middle', () => {
+        test('it returns the element at that index', () => {
+            const ll = LinkedList.fromValues(10, 20, 30, 40)
+
+            expect(ll.getByIndex(2).value).toBe(30)
         })
     })
 })
+
