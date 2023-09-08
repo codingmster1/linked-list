fe151bd191566be8fbcfd8af025e9415bd58d483
Jeff <doylejeff98@gmail.com>
2023-09-08 13:54:48 -0400

    print at index to insert at index

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ const LinkedList = require('./LinkedList')
 
 const ll = LinkedList.fromValues(10, 20, 30, 40)
 ll.print()
-console.log(ll.getByIndex(2).value)
+ll.insertAtIndex(2, 60)
+ll.print()
+
+/*console.log(ll.getByIndex(2).value)*/
